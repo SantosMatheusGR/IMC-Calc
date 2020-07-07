d7517cda235ea21a0e1e3331383bdb849f718960
Matheus Santos <53152862+SantosMatheusGR@users.noreply.github.com>
2020-07-06 21:55:29 -0300

    Calculo e IMC e ajustes na responsividad

--- content/js/index.js
+++ content/js/index.js
@@ -45,5 +45,33 @@ function validarInput(data){
 }
 
 function calculaImc(data){
-    
+    var imc = data.peso / (data.altura * data.altura);
+
+    var posicaoTabela = verificarPosicaoTabelaImc(imc);
+
+    resultado.innerHTML = (`<ul>
+                            <li>O seu IMC é de <strong>${imc.toFixed(2)}</strong>.</li>
+                            <li>Seu IMC se encontra na posição <strong>${posicaoTabela}</strong> na tabela de condição.</li>
+                        </ul>`);
+}
+
+function verificarPosicaoTabelaImc(imc){
+    if(imc < 18.5){
+        return "Abaixo Do Peso";
+    }
+    else if(imc >= 18.5 && imc <= 24.99){
+        return "Peso Ideal";
+    }
+    else if(imc >= 25 && imc <= 29.99){
+        return "Sobrepeso";
+    }
+    else if(imc >= 30 && imc <= 34.99){
+        return "Obesidade Grau 1";
+    }
+    else if(imc >= 35 && imc <= 39.99){
+        return "Obesidade Grau 2";
+    }
+    else if(imc > 40){
+        return "Obesidade Grau 3";
+    }
 }
